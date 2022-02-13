a838160f67ea2560699c4112280887288e77a361
jaylearnjs <jaylearnjs@gmail.com>
2022-02-13 11:10:46 +0530

    Function Declaration and Expression

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ if (hasDriversLicense) console.log('I can drive :D');
 // const private = 534;
 */
 
+/*
 //Functions
 function logger() {
   console.log("My Name is Abcd");
@@ -29,3 +30,18 @@ function fruitProcessor(apples, oranges) {
 const applejuice = fruitProcessor(5, 2);
 console.log(applejuice);
 console.log(fruitProcessor(4,2));
+*/
+
+// Function Declaration
+function calcAge1(birthYear) {
+  return 2030 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+
+// Function Expression
+const calcAge2 = function (birthYear) {
+  return 2030 - birthYear;
+};
+const age2 = calcAge2(1991);
+console.log(age1, age2);
