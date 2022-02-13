f97c82d088c0e173e61b7695a853df11fdec8810
jaylearnjs <jaylearnjs@gmail.com>
2022-02-13 10:53:59 +0530

    Functions

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-
+/*
 // Activating Strict Mode
 let hasDriversLicense = false;
 const passTest = true;
@@ -10,3 +10,22 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 // const interface = 'Audio';
 // const private = 534;
+*/
+
+//Functions
+function logger() {
+  console.log("My Name is Abcd");
+}
+
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+    const juice = `Juice with ${apples} and ${oranges} oranges.`;
+    return juice
+}
+
+
+const applejuice = fruitProcessor(5, 2);
+console.log(applejuice);
+console.log(fruitProcessor(4,2));
